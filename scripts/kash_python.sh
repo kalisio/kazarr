5be2193c30413174867c2a7aed9367b88a78f6ae
Floran05 <carvalhofloran@gmail.com>
2026-06-04 17:09:29 +0200

    wip: Add continuous integration for tests and code analysis (#15)

--- scripts/kash_python.sh
+++ scripts/kash_python.sh
@@ -1,51 +1,54 @@
 MICROMAMBA_VERSION="latest"
 
 install_micromamba() {
-  export PREFIX_LOCATION="${PREFIX_LOCATION:-${HOME}/micromamba}"
-  # Mandatory for micromamba to work post installation
-  export MAMBA_ROOT_PREFIX="$PREFIX_LOCATION"
-  export BIN_FOLDER="${BIN_FOLDER:-${HOME}/.local/bin}"
-  export INIT_YES="no" 
-  export CONDA_FORGE_YES="yes"
-  
-  curl -Lso /tmp/install_micromamba.sh https://micro.mamba.pm/install.sh
-  bash /tmp/install_micromamba.sh < /dev/null
-  
-  export MAMBA_EXE="${BIN_FOLDER}/micromamba"
-  export PATH="$(dirname "$MAMBA_EXE"):$PATH"
-  eval "$("$MAMBA_EXE" shell hook -s bash)"
+    export PREFIX_LOCATION="${PREFIX_LOCATION:-${HOME}/micromamba}"
+    # Mandatory for micromamba to work post installation
+    export MAMBA_ROOT_PREFIX="$PREFIX_LOCATION"
+    export BIN_FOLDER="${BIN_FOLDER:-${HOME}/.local/bin}"
+    export INIT_YES="no" 
+    export CONDA_FORGE_YES="yes"
+
+    curl -Lso /tmp/install_micromamba.sh https://micro.mamba.pm/install.sh
+    bash /tmp/install_micromamba.sh < /dev/null
+
+    export MAMBA_EXE="${BIN_FOLDER}/micromamba"
+    export PATH="$(dirname "$MAMBA_EXE"):$PATH"
+    eval "$("$MAMBA_EXE" shell hook -s bash)"
 }
 
 setup_micromamba_env() {
-  local env_name="$1"
-  local env_file="${2:-environment.yml}" 
-  local auto_activate="${3:-false}"
+    # Ensure root prefix is set for micromamba, even if CI has changed its shell
+    export MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-${HOME}/micromamba}"
 
-  if [ -z "$env_name" ]; then
-    echo "Error: Environment name is required."
-    return 1
-  fi
+    local env_name="$1"
+    local env_file="${2:-environment.yml}" 
+    local auto_activate="${3:-false}"
 
-  if [ ! -f "$env_file" ]; then
-    echo "Error: The file '$env_file' is not found."
-    return 1
-  fi
-
-  echo "Creating environment '$env_name' from '$env_file'..."
-  micromamba create -f "$env_file" -n "$env_name" -y
-
-  if [ $? -eq 0 ]; then
-    echo "Environment '$env_name' created and configured successfully !"
-    
-    if [ "$auto_activate" = "true" ] || [ "$auto_activate" = "1" ]; then
-      echo "Automatic activation of the environment '$env_name'..."
-      eval "$(micromamba shell hook --shell bash)"
-      micromamba activate "$env_name"
+    if [ -z "$env_name" ]; then
+        echo "Error: Environment name is required."
+        return 1
     fi
-  else
-    echo "Error occurred while creating the environment."
-    return 1
-  fi
+
+    if [ ! -f "$env_file" ]; then
+        echo "Error: The file '$env_file' is not found."
+        return 1
+    fi
+
+    echo "Creating environment '$env_name' from '$env_file'..."
+    micromamba create -f "$env_file" -n "$env_name" -y
+
+    if [ $? -eq 0 ]; then
+        echo "Environment '$env_name' created and configured successfully !"
+        
+        if [ "$auto_activate" = "true" ] || [ "$auto_activate" = "1" ]; then
+        echo "Automatic activation of the environment '$env_name'..."
+        eval "$(micromamba shell hook --shell bash)"
+        micromamba activate "$env_name"
+        fi
+    else
+        echo "Error occurred while creating the environment."
+        return 1
+    fi
 }
 
 run_pytest() {
